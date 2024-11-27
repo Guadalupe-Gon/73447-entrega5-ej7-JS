51584a793fbc70a44c0d51d6aa6d4767c94a29d5
Guadalupe Gonçalves <guadalupe.goncalves@gmail.com>
2024-11-26 21:09:19 -0300

    archivo de ejemplo eliminado

--- JS/ejercicio.js
+++
@@ -1,8 +0,0 @@
-const miListaDePendientes = ["Outlander", "La Emperatriz", "Regreso a Salem's Lot", "It"];
-
-miListaDePendientes.push("Better call Saul");
-
-miListaDePendientes[1] = "The Last of Us";
-
-// Muestra el array actualizado por consola
-console.log(miListaDePendientes);
